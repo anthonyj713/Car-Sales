1293535199ba37c8a13d6a1630ff63624bedff75
Anthony Johnson <wontons713@Anthonys-MacBook-Pro.local>
2020-02-20 08:18:25 -0700

    Finished!

--- src/index.js
+++ src/index.js
@@ -5,5 +5,15 @@ import App from './App';
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { Reducer } from './components/reducers/index';
+
+const store = createStore(Reducer);
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+<Provider store={store}>
+<App />
+</Provider>
+, rootElement);
